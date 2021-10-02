c30737b9a5cc39f00443c46e446dea0484aab090
Rodrigo Lemos <rodrigoflemos@gmail.com>
2021-10-02 15:14:26 -0300

    Testes iniciais

---
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -0,0 +1,36 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+
+        assertEquals(new BigDecimal("0.00"), bonus);
+    }
+
+    @Test
+    void bonusDeveriaSer10PorcentoDoSalario() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500")));
+
+        assertEquals(new BigDecimal("250.00"), bonus);
+    }
+
+    @Test
+    void bonusDeveriaSer10PorCentoParaSalarioDeExatamente10000() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10000")));
+
+        assertEquals(new BigDecimal("1000.00"), bonus);
+    }
+}
